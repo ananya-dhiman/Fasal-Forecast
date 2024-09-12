94d3a8eccb70d918370a9c3253f873be4909711c
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2024-09-12 23:21:05 +0530

    commiting backend files

---
+++ FasalForecast/environmental_data.py
@@ -0,0 +1,55 @@
+import requests
+
+
+def get_location():
+    # sending request to ipinfo.io to get location data
+    response = requests.get('https://ipinfo.io')
+
+    if response.status_code == 200:
+        print(f"{response=}")
+        data = response.json()
+        print(f"{data=}")
+        location = data.get('loc')
+        city = data.get('city')
+        print(f"{location=}")# Returns 'lat,lng' as a string
+
+        if location:
+            latitude, longitude = location.split(',')
+            return float(latitude), float(longitude), str(city)
+        else:
+            return None
+    else:
+        return None
+
+
+def get_weather_data(lat, lon, api_key):
+    # URL for OpenWeatherMap API with specified latitude and longitude
+    url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+
+    # Sending GET request to OpenWeatherMap API
+    response = requests.get(url)
+
+    # If the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return f"Error: Unable to fetch data (Status code: {response.status_code})"
+
+
+# Example usage
+def get_data():
+    location = get_location()
+    if location:
+        print(f"Latitude: {location[0]}, Longitude: {location[1]}, City: {location[2]}")
+    else:
+        print("Could not retrieve location")
+    latitude = location[0]  # Replace with the latitude you want to query
+    longitude = location[1]  # Replace with the longitude you want to query
+    api_key = "5121b5175e1e47be62df471453167da2"  # Replace with your actual API key
+
+    weather_data = get_weather_data(latitude, longitude, api_key)
+    # print(weather_data)
+    needed_data = [weather_data.get('main', {}).get('temp_min'), weather_data.get('main', {}).get('temp_max'), weather_data.get('main', {}).get('humidity'), weather_data.get('wind', {}).get('speed')]
+    # print(needed_data)
+    return {'temp_min':needed_data[0], 'temp_max':needed_data[1], 'humidity':needed_data[2], 'wind_speed':needed_data[3]}
---
+++ FasalForecast/fasal_forecast/routes.py
@@ -0,0 +1,42 @@
+from fasal_forecast import app
+from flask import jsonify, request
+import os
+from prediction_model import predict_disease
+from environmental_data import get_data
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return "home page"
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    # checking if request has a file
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
+
+    # checking if no file
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+
+    if file:
+        # saving the image file
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg')
+        file.save(file_path)
+
+        # return appropriate response if request was successful
+        return jsonify({
+            'message': 'File successfully uploaded',
+            'file_path': file_path
+        }), 200
+
+
+@app.route('/predict')
+def make_prediction():
+    return jsonify(predict_disease())
+
+# temp_min , temp_max , humidity and wind speed
+@app.route('/envdata')
+def get_env_data():
+    return jsonify(get_data())
